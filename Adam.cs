2ee767cdeca8c20251bbdcf0c658331ae5f30f0c
Ebemhen <116885295+Ebemhen@users.noreply.github.com>
2022-12-17 06:55:11 +0100

    Create Adam.cs

---
+++ Adam.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AdamStepsApp
+{
+    class CheckSteps
+    {   
+        public int count;
+        public string steps;
+        public void checkStep(steps){
+            foreach(int ch in steps)
+                {
+                    If(ch == 'U'){
+                        count++;
+                    }
+                }
+             Console.WriteLine(count) ;
+        }
+    }
+
+     class Program
+     {
+        public int testcases; public string stepInput;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of test cases");
+            testcases = int.Parse(Console.ReadLine());
+            for(int j = 0; j < testcases; j++)
+            {
+               Console.WriteLine("Enter steps:");
+               stepsInput = Console.ReadLine();
+               CheckSteps StepCase = new CheckSteps();
+               StepCase.checkStep(stepsInput);
+                
+
+            }
+        }
+    }
+}
